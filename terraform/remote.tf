a20718dd9a4087501350e5e9721f1d8e5d56412f
epicatto <epicatto@velocitypartners.net>
2020-08-09 22:08:31 -0300

    Enable TF apply command on master push

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -3,7 +3,6 @@ terraform {
 
   backend "remote" {
     organization = "ezedev"
-    token = "MEdEGQwuyxPv9g.atlasv1.f9DWi0b6D1vFy6PI3Pp7JQmDYIMT0PgzMhozr2zfvIxKvp3oPK5eWD8mw7M9IaOMNmw"
 
     workspaces {
       name = "fastapi-test-dev"
